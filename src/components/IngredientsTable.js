df16eceb04e469f3621776e5b348c884026c8691
“bhana6491” <bhana6491@gmail.com>
2024-12-17 13:23:43 -0500

    adding ingredients to storageSession for cache. Next js only caches server side.

--- src/components/IngredientsTable.js
+++ src/components/IngredientsTable.js
@@ -8,6 +8,7 @@ import {NutrientsTable} from "./utils";
 import RecipeSummary from "./RecipeSummary";
 const { Title } = Typography;
 import { Flex, Spin } from 'antd';
+import { revalidatePath } from "next/cache";
 
 const IngredientsTable = (petInfo) => {
   const [dataSource, setDataSource] = useState([]);
@@ -270,13 +271,32 @@ const IngredientsTable = (petInfo) => {
     },
   ];
   useEffect(() => {
-    // Make API request here and update the dataSource state
-    fetch(
-      "https://2kj1u5y1yh.execute-api.us-east-1.amazonaws.com/Testing/ingredients"
-    )
-      .then((response) => response.json())
-      .then((data) => setDataSource(JSON.parse(data.body)));
+    const fetchData = () => {
+      try {
+        const cachedData = sessionStorage.getItem("ingredientsData");
+        if (cachedData) {
+          setDataSource(JSON.parse(cachedData));
+        } else {
+          fetch(
+            "https://2kj1u5y1yh.execute-api.us-east-1.amazonaws.com/Testing/ingredients",
+          )
+            .then(response => response.json())
+            .then(data => {
+              setDataSource(JSON.parse(data.body));
+              sessionStorage.setItem("ingredientsData", data.body);
+            })
+            .catch(error => {
+              console.error("Error fetching data:", error);
+            });
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
   }, []);
+  // setDataSource(JSON.parse(data.body));
 
   while (!dataSource || dataSource.length === 0) {
     return (
